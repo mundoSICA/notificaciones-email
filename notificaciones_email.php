8585da03376c2b58c5b31fbf00d74d7f6977a817
Fitorec <chanerec@gmail.com>
2011-10-07 12:34:32 -0500

    Agregando comentarios en el código a utilizar

--- notificaciones_email.php
+++ notificaciones_email.php
@@ -11,14 +11,14 @@
  * @package       NotificacionesEmail
  * @Description   Envia notificaciones por email realizando validaciones previas.
  * @copyright     2011, SICÁ - Soluciones Integrales en Computación Áplicada.
- * @link          http://mundosica.com/notificaciones_email
+ * @link          https://github.com/mundoSICA/notificaciones-email
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  * @since         NotificacionesEmail v 0.1Alpha
  * 
  */
 
 class NotificacionesEmail {
-	/* variables to use */
+	/** variables a usar **/
 	var $msg = '';
 	var $errors = array();
 	var $config = null;
@@ -26,46 +26,63 @@ class NotificacionesEmail {
 	/************************************************************************************
 	 *                             Functiones de validación                             *
 	 ************************************************************************************/
-	function validText($str='') {
-		$validChars = 'a-z0-9 .,áéíóúüÁÉÍÓÚÜñÑ#';
-		return preg_match('/^[' . $validChars . ']*$/i',$str);
-	}
-	function validEmail($mail='') {
-		return preg_match('/^[A-Z0-9._%-]+@[a-z][A-Z0-9.-]+\\.[A-Z]{2,4}$/i',$mail);
-	}
+	 
 	/**
-	* Check that a value is a valid phone number.
+	* Revisa si el campo check es texto, sin caracteres especiales.
 	* 
-	* @param string $phone value to check
+	* @param string $check el valor a revisar
 	* @return  boolean Success
-	* @access public
-	* @link 
+	* @access publico
+	* @link https://github.com/mundoSICA/notificaciones-email
 	*/
-	function validPhone($phone=null) {
-		$validChars = '-0-9\s\(\)';
-		return preg_match('/^[' . $validChars . ']{7,30}$/i',$phone);
+	function validText($check=null) {
+		$validChars = 'a-z0-9 .,áéíóúüÁÉÍÓÚÜñÑ#';
+		return preg_match('/^[' . $validChars . ']*$/i',$check);
 	}
 	/**
-	* Check that a value is a valid human DateTime.
+	* Revisa si el campo check corresponde a una dirección de correo valida.
+	* 
+	* @param string $check el valor a revisar.
+	* @return  boolean Success
+	* @access publico
+	* @link https://github.com/mundoSICA/notificaciones-email
+	*/
+	function validEmail($check=null) {
+		return preg_match('/^[A-Z0-9._%-]+@[a-z][A-Z0-9.-]+\\.[A-Z]{2,4}$/i',$check);
+	}
+	/**
+	* Revisa si el campo check es un número telefonico valido
+	* 
+	* @param string $check el valor a revisar.
+	* @return  boolean Success
+	* @access publico
+	* @link https://github.com/mundoSICA/notificaciones-email
+	*/
+	function validPhone($check=null) {
+		$validChars = '-0-9\s\(\)';
+		return preg_match('/^[' . $validChars . ']{7,30}$/i',$check);
+	}
+	/**
+	* Revisa si el campo check es una fecha generada por el Plugin JQueryDateTimePicker.
 	*
-	* @param string $dateTime value to check
-	* @return boolean Success
-	* @access public
+	* @param string $check value to check
+	* @return boolean Resultado
+	* @access publico
 	* @link http://www.projectcodegen.com/JQueryDateTimePicker.aspx
 	*/
-	function validDateTime( $dateTime ) {
+	function validDateTime( $check ) {
 		$regex = '%^(0[1-9]|1[0-2])/(0[1-9]|[1-3][0-9])/([1-2][0-9]{3}) (0[1-9]|1[0-2]):[0-5][0-9] (AM|PM)%';
-		return preg_match($regex,$dateTime);
+		return preg_match($regex,$check);
 	}
 	/**
-	* Revisa si la URL es valida.
+	* Revisa si el campo check corresponde a una URL valida.
 	*
-	* @param string $URL valor a revisar
+	* @param string $check valor a revisar
 	* @return boolean Resultado
-	* @access public
-	* @link
+	* @access publico
+	* @link https://github.com/mundoSICA/notificaciones-email
 	*/
-	function ValidUrl( $url ) {
+	function ValidUrl( $check ) {
 		$validChars = '([' . preg_quote('!"$&\'()*+,-.@_:;=~[]') . '\/0-9a-z\p{L}\p{N}]|(%[0-9a-f]{2}))';
 		$hostname = '(?:[a-z0-9][-a-z0-9]*\.)*(?:[a-z0-9][-a-z0-9]{0,62})\.(?:(?:[a-z]{2}\.)?[a-z]{2,4}|museum|travel)';
 		$regex = '/^(?:(?:https?|ftps?|file|news|gopher):\/\/)' . '(?:' . $hostname . ')' .
@@ -73,7 +90,7 @@ class NotificacionesEmail {
 			'(?:\/?|\/' . $validChars . '*)?' .
 			'(?:\?' . $validChars . '*)?' .
 			'(?:#' . $validChars . '*)?$/iu';
-		return preg_match($regex,$url);
+		return preg_match($regex,$check);
 	}
 	/**
 	 * Init to object
@@ -81,6 +98,7 @@ class NotificacionesEmail {
 	 * @param array $config arreglo de configuracion
 	 * @return void
 	 * @access publico
+	 * @link https://github.com/mundoSICA/notificaciones-email
 	 */
 	public function __construct($config) {
         	$this->configurar($config);
@@ -91,6 +109,7 @@ class NotificacionesEmail {
 	 * @param array $config arreglo de configuracion
 	 * @return void
 	 * @access publico
+	 * @link https://github.com/mundoSICA/notificaciones-email
 	 */
     public function configurar($config) {
         $this->config = $config;
@@ -102,7 +121,7 @@ class NotificacionesEmail {
 	 * @param tipo $parametro1 descripción del párametro 1.
 	 * @return Boolean el resultado de la acción.
 	 * @access publico
-	 * @link
+	 * @link https://github.com/mundoSICA/notificaciones-email
 	 */
 	function sendMails() {
 		$typesValidations = array(
